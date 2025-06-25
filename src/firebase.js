6bd0ec135c810231d84ca78e2ee25e9dc91225a7
Gayatri Rane <ranegayatri625@gmail.com>
2025-06-25 12:42:17 +0530

    your message

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "fullstack-twitter-4d8a5.firebaseapp.com",
-  projectId: "fullstack-twitter-4d8a5",
-  storageBucket: "fullstack-twitter-4d8a5.firebasestorage.app",
-  messagingSenderId: "628971384743",
-  appId: "1:628971384743:web:8d3e6aa8ce5803125ed082"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
