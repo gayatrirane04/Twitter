d590d1dd0f17e7bb8ff59e757d21ee390411ea0b
Gayatri Rane <ranegayatri625@gmail.com>
2025-07-09 15:37:28 +0530

    Update Firebase config or environment usage

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: "twitter-94363.firebaseapp.com",
+  projectId: "twitter-94363",
+  storageBucket: "twitter-94363.firebasestorage.app",
+  messagingSenderId: "740998318791",
+  appId: "1:740998318791:web:c80713470fddccec1b4ce2"
 };
 
 // Initialize Firebase
